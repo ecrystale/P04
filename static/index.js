e35b49d4e4beda4bc3741eb83bb24cc94510d4cc
tina <tinaw425@gmail.com>
2019-03-31 22:47:06 -0400

    added axes

--- static/index.js
+++ static/index.js
@@ -2,7 +2,7 @@ var width = 1250, height = 550;
 
 var chart = d3.select(".chart")
     .attr("width", width)
-    .attr("height",height)
+    .attr("height", height)
 
 // Get the correctly organized data from python
 var data = JSON.parse(document.getElementsByClassName("bar_data")[0].innerHTML);
@@ -15,6 +15,20 @@ Each bar will take up 2 or 3 months (Ex. One bar can be Jan-March 2011, next one
 User can narrow down on time, console, genre, ...
 Overall, there will be 40 bars (We can change it later if we want)
 */
+
+var x_scale = d3.scaleLinear()
+    .domain([0,40])
+    .range([0, 1000]);
+
+var y_scale = d3.scaleLinear()
+    .domain([0,100])
+    .range([350,0]);
+
+var x_axis = d3.axisBottom()
+    .scale(x_scale)
+
+var y_axis = d3.axisLeft()
+    .scale(y_scale)
 
 var bar = chart.selectAll("g")
     .data(data)
@@ -36,5 +50,13 @@ var bar = chart.selectAll("g")
     .on("click", function(d) {
          console.log(d);
     });
+
+chart.append('g')
+    .attr("transform","translate (50,420)")
+    .call(x_axis)
+
+chart.append('g')
+    .attr("transform","translate (50,70)")
+    .call(y_axis)
 
 console.log(bar);
